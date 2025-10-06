78e62a0bbc7e34195c8ea807441a7646f3980ade
hiiamboris <hiiamboris@gmail.com>
2025-10-06 23:23:57 +0800

    FIX: minor shell fixes

---
+++ docs/community/articles/refresh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+~/dataset/scrape-page.sh https://web.archive.org/web/20230323034804/https://www.notamonadtutorial.com/interview-with-nenad-rakocevic-about-red-a-rebol-inspired-programming-language/ nenad-interview.md
+~/dataset/scrape-page.sh https://en.wikipedia.org/wiki/Red_%28programming_language%29 wikipedia.md
--- docs/community/refresh.sh
+++ docs/community/refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/dataset/scrape-code https://github.com/red/red.wiki wiki
+~/dataset/scrape-code.sh https://github.com/red/red.wiki wiki
 wget -O specs/red-specs-meijeru.adoc https://raw.githubusercontent.com/meijeru/red.specs-public/refs/heads/master/specs.adoc
